86e08c20ccaf1d2184e8166c68a65d84bda30a4a
Oliver Kleinecke <kleinecke.oliver@googlemail.com>
2025-11-27 13:56:58 +0000

    Complete multi-instance TinyUSB implementation
    Architectural fixes:
    - OSAL queue/spinlock moved to file scope (cannot embed in struct)
    - Separate queues and spinlocks for each rhport
    - Instances store only pointers to OSAL resources
    
    Type and API fixes:
    - Add missing fields to usbh_device_t (addressed bit, ep_status, ep_callback)
    - Include tusb_private.h for tu_edpt_state_t type
    - Add usbh_class_driver_t type definition to header
    - Add all required function declarations
    - Add TU_LOG_USBH macros
    
    All critical compilation blockers should now be resolved.

--- src/host/usbh_pvt.h
+++ src/host/usbh_pvt.h
@@ -30,10 +30,36 @@
 #include "usbh.h"
 #include "hcd.h"
 #include "osal/osal.h"
+#include "common/tusb_private.h"
 
 #ifdef __cplusplus
  extern "C" {
 #endif
+
+#define TU_LOG_USBH(...)      TU_LOG(CFG_TUH_LOG_LEVEL, __VA_ARGS__)
+#define TU_LOG_MEM_USBH(...)  TU_LOG_MEM(CFG_TUH_LOG_LEVEL, __VA_ARGS__)
+#define TU_LOG_BUF_USBH(...)  TU_LOG_BUF(CFG_TUH_LOG_LEVEL, __VA_ARGS__)
+#define TU_LOG_INT_USBH(...)  TU_LOG_INT(CFG_TUH_LOG_LEVEL, __VA_ARGS__)
+#define TU_LOG_HEX_USBH(...)  TU_LOG_HEX(CFG_TUH_LOG_LEVEL, __VA_ARGS__)
+
+//--------------------------------------------------------------------+
+// Class Driver API
+//--------------------------------------------------------------------+
+
+typedef struct {
+  char const* name;
+  bool (* const init       )(void);
+  bool (* const deinit     )(void);
+  bool (* const open       )(uint8_t rhport, uint8_t dev_addr, tusb_desc_interface_t const * itf_desc, uint16_t max_len);
+  bool (* const set_config )(uint8_t dev_addr, uint8_t itf_num);
+  bool (* const xfer_cb    )(uint8_t dev_addr, uint8_t ep_addr, xfer_result_t result, uint32_t xferred_bytes);
+  void (* const close      )(uint8_t dev_addr);
+} usbh_class_driver_t;
+
+// Invoked when initializing host stack to get additional class drivers.
+// Can be implemented by application to extend/overwrite class driver support.
+// Note: The drivers array must be accessible at all time when stack is active
+usbh_class_driver_t const* usbh_app_driver_get_cb(uint8_t* driver_count);
 
 //--------------------------------------------------------------------+
 // Instance Structure Definition
@@ -48,6 +74,7 @@ typedef struct {
   uint8_t hub_port;
   uint8_t speed;
 
+  // Device Descriptor
   uint16_t bcdUSB;
   uint8_t bDeviceClass;
   uint8_t bDeviceSubClass;
@@ -61,18 +88,28 @@ typedef struct {
   uint8_t iSerialNumber;
   uint8_t bNumConfigurations;
 
-  volatile uint8_t state;
-  uint8_t itf_count;
-  uint8_t ep_count;
-
-  uint8_t itf2drv[CFG_TUH_INTERFACE_MAX];
-  uint8_t ep2drv[CFG_TUH_ENDPOINT_MAX][2];
-
+  // Device State
   struct TU_ATTR_PACKED {
-    volatile bool connected : 1;
-    volatile bool configured : 1;
-    volatile bool suspended : 1;
+    volatile uint8_t connected  : 1; // After 1st transfer
+    volatile uint8_t addressed  : 1; // After SET_ADDR
+    volatile uint8_t configured : 1; // After SET_CONFIG and all drivers are configured
+    volatile uint8_t suspended  : 1; // Bus suspended
   };
+
+  // Endpoint & Interface mapping
+  uint8_t itf2drv[CFG_TUH_INTERFACE_MAX];  // map interface number to driver (0xff is invalid)
+  uint8_t ep2drv[CFG_TUH_ENDPOINT_MAX][2]; // map endpoint to driver (0xff is invalid)
+
+  // Endpoint status tracking
+  tu_edpt_state_t ep_status[CFG_TUH_ENDPOINT_MAX][2];
+
+#if CFG_TUH_API_EDPT_XFER
+  // Endpoint transfer callbacks
+  struct {
+    tuh_xfer_cb_t complete_cb;
+    uintptr_t user_data;
+  } ep_callback[CFG_TUH_ENDPOINT_MAX][2];
+#endif
 } usbh_device_t;
 
 // Control transfer info
@@ -120,6 +157,40 @@ struct usbh_instance {
 };
 
 typedef struct usbh_instance usbh_instance_t;
+
+//--------------------------------------------------------------------+
+// Internal Helper Functions
+//--------------------------------------------------------------------+
+
+// Get rhport from device address
+uint8_t usbh_get_rhport(uint8_t daddr);
+
+// Get enumeration buffer
+uint8_t* usbh_get_enum_buf(void);
+
+// Interrupt control
+void usbh_int_set(bool enabled);
+
+// Spinlock control
+void usbh_spin_lock(bool in_isr);
+void usbh_spin_unlock(bool in_isr);
+
+// Defer a function to be called later
+void usbh_defer_func(osal_task_func_t func, void *param, bool in_isr);
+
+// Driver enumeration complete callback
+void usbh_driver_set_config_complete(uint8_t dev_addr, uint8_t itf_num);
+
+// Endpoint transfer functions
+#if CFG_TUH_API_EDPT_XFER
+bool usbh_edpt_xfer_with_callback(uint8_t dev_addr, uint8_t ep_addr, uint8_t * buffer, uint16_t total_bytes,
+                                  tuh_xfer_cb_t complete_cb, uintptr_t user_data);
+#endif
+
+bool usbh_edpt_xfer(uint8_t dev_addr, uint8_t ep_addr, uint8_t * buffer, uint16_t total_bytes);
+bool usbh_edpt_claim(uint8_t dev_addr, uint8_t ep_addr);
+bool usbh_edpt_release(uint8_t dev_addr, uint8_t ep_addr);
+bool usbh_edpt_busy(uint8_t dev_addr, uint8_t ep_addr);
 
 #ifdef __cplusplus
  }
